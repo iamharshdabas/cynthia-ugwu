319dda90215ae76265b421fbc92e1523ec1d6a2e
Harsh Dabas <iamharshdabas@gmail.com>
2023-09-13 21:58:18 +0530

    add: gsap and hero page animation

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
+import gsap from 'gsap'
+import LocomotiveScroll from 'locomotive-scroll'
 import './locomotive-scroll.css'
 import './style.css'
-
-import LocomotiveScroll from 'locomotive-scroll'
 
 const scroll = new LocomotiveScroll({
   el: document.querySelector('main'),
   smooth: true,
 })
+
+const time = gsap.timeline()
+
+time
+  .from('.up1', { y: '100%', duration: 1 })
+  .from('.up2', { y: '100%', duration: 1 })
+  .from('.down1', { y: '-100%', duration: 1 })
+  .from('.down2', { y: '-100%', duration: 1 })
+  .from('.fade', { opacity: 0, duration: 1 })
